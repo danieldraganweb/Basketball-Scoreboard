1e3251d8629ac0c57db39e7816e64b577f5b286b
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-02-15 12:50:44 +0100

    functions maped with html

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 let homeScore = document.getElementById("home-score");
 let awayScore = document.getElementById("away-score");
+let quarter = document.getElementById("quarter");
+let timer = document.getElementById("timer");
+let homeFouls = document.getElementById("home-fouls-count");
+let awayFouls = document.getElementById("away-fouls-count");
 
-function add1HomeScore() {}
-function add2HomeScore() {}
-function add3HomeScore() {}
+function add1HomeScore() {
+  console.log("add1HomeScore() called");
+}
+function add2HomeScore() {
+  console.log("add2HomeScore() called");
+}
+function add3HomeScore() {
+  console.log("add3HomeScore() called");
+}
 
-function add1AwayScore() {}
-function add2AwayScore() {}
-function add3AwayScore() {}
-function startTimer() {}
-function stopTimer() {}
-function resetTimer() {}
+function add1AwayScore() {
+  console.log("add1AwayScore() called");
+}
+function add2AwayScore() {
+  console.log("add2AwayScore() called");
+}
+function add3AwayScore() {
+  console.log("add3AwayScore() called");
+}
 
-function addQuarter() {}
+function startTimer() {
+  console.log("startTimer() called");
+}
+function stopTimer() {
+  console.log("stopTimer() called");
+}
+function resetTimer() {
+  console.log("resetTimer() called");
+}
+
+function addQuarter() {
+  console.log("addQuarter() called");
+}
