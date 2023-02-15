11dd5bbc743bdf5e6f4605204287e15ef8f34585
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-02-15 09:51:59 +0100

    btns for stats board

---
+++ index.js
@@ -0,0 +1,15 @@
+let homeScore = document.getElementById("home-score");
+let awayScore = document.getElementById("away-score");
+
+function add1HomeScore() {}
+function add2HomeScore() {}
+function add3HomeScore() {}
+
+function add1AwayScore() {}
+function add2AwayScore() {}
+function add3AwayScore() {}
+function startTimer() {}
+function stopTimer() {}
+function resetTimer() {}
+
+function addQuarter() {}
